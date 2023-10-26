324399d3fe9b55d79a55f4be739065c11b6385f7
paulohcatto@gmail.com <paulohcatto@gmail.com>
2023-10-26 18:45:05 -0300

    criando carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let total = 0;
+
+limpar();
+
+function adicionar() {
+    let produto = document.getElementById("produto").value;
+    let quantidade = document.getElementById('quantidade').value;
+    let listaCarrinho = document.getElementById('lista-produtos');
+    let valorTotal = document.getElementById('valor-total');
+
+    if (produto) {
+        let valorUnitario = parseFloat(produto.split('R$')[1]);
+        total += quantidade * valorUnitario;
+
+        listaCarrinho.innerHTML += `
+        <section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}X</span> Celular <span class="texto-azul">R$${produto}</span>
+        </section>`;
+
+        valorTotal.textContent = `R$ ${total.toFixed(2)}`;
+    }
+}
+
+function limpar() {
+    total = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0.00';
+}
